ba70c2383f1ff5560b192419e1b7f849b2aff1b2
isMentor <15026864008@163.com>
2021-12-09 14:55:17 +0800

    [build] 0.0.1

--- release.sh
+++ release.sh
@@ -6,33 +6,20 @@
 
 # 执行过程不为 true 自动退出
 set -e
-
 echo "Enter release version: "
-
 read VERSION
-
 read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
-
 echo # (optional) move to a new line
-
 if [[ $REPLY =~ ^[Yy]$ ]]
-
 then
-
   echo "Releasing $VERSION ..."
 
   #commit
-
   git add -A
-  
   git commit -m "[build] $VERSION"
-
   npm version $VERSION --message "[release] $VERSION"
-
   git push origin master
-
   # publish
 
   npm publish
-  
 fi
